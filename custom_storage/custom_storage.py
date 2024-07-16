f5ac8931d26afdf019317b95fcb663da67df2a76
nicarbnig <eonyenaucheya@nicarb.org>
2024-07-16 09:13:51 +0100

    Update custom_storage.py

--- custom_storage/custom_storage.py
+++ custom_storage/custom_storage.py
@@ -1,7 +1,8 @@
 from whitenoise.storage import CompressedManifestStaticFilesStorage
 
 class CustomStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    def url(self, name, *args, **kwargs):
-        if name.endswith('.map'):
-            return None  # Ignore .map files
-        return super().url(name, *args, **kwargs)
+    def post_process(self, *args, **kwargs):
+        files_to_ignore = [name for name in self.hashed_files if name.endswith('.map')]
+        for name in files_to_ignore:
+            del self.hashed_files[name]
+        return super().post_process(*args, **kwargs)
